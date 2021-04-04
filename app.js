e16731930daa7fb39f805966ae9b424b10ac32e8
craigkehl <keh19003@byui.edu>
2021-04-03 23:57:34 -0600

    JWT integration backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const DATABASE_URL = require('dotenv').config();
 
+
 const PORT = process.env.PORT || 5100;
 
 const app = express();
@@ -24,5 +25,14 @@ app.use((req, res, next) => {
 app.use('/auth/', authRoutes);
 app.use(meetingsRoutes);
 app.use(adminRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
 console.log('Up and running')
 app.listen(PORT);
